ade86ba016ccfc523e9e35a01d3efde5293fd4cd
Artem Tanyhin <magic.cat.775@gmail.com>
2023-04-27 21:56:14 -0400

    mui setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { AppBar, Box, Button, Typography } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  function classify() {
+    console.log('Classify');
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppBar position='static'>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 2, my: 1 }}>
+          Blood Cell Classifier
+        </Typography>
+      </AppBar>
+      <Box sx={{ my: 2, mx: 2 }}>
+
+        <Button variant="outlined" component="label">
+          Upload image
+          <input hidden accept="image/*" type="file" />
+        </Button>
+
+        <Button variant="contained" onClick={()=>{classify()}}>Classify</Button>
+      </Box>
     </>
   )
 }
 
-export default App
+export default App;
